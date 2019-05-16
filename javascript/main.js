d33d1ddfee86e4c7f57bad0e6ddf90e976783965
mendako <v1758219@oita-u.ac.jp>
2019-05-16 11:11:04 +0900

    add: tree.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,40 @@
 "use strict"
+    // ページの読み込みを待つ
+    window.addEventListener('load', init);
 
-window.onload = function(){
-  let mainCanvas = document.getElementById("mainCanvas")
-  let mainCanvasContext = mainCanvas.getContext("2d")
-  mainCanvasContext.fillText("hello world", 0, 60)
-}
+    function init() {
+
+      // サイズを指定
+      const width = 960;
+      const height = 540;
+
+      // レンダラーを作成
+      const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#myCanvas')
+      });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(width, height);
+
+      // シーンを作成
+      const scene = new THREE.Scene();
+
+      // カメラを作成
+      const camera = new THREE.PerspectiveCamera(45, width / height);
+      camera.position.set(0, 0, +1000);
+
+      // 箱を作成
+      const geometry = new THREE.BoxGeometry(400, 400, 400);
+      const material = new THREE.MeshNormalMaterial();
+      const box = new THREE.Mesh(geometry, material);
+      scene.add(box);
+
+      tick();
+
+      // 毎フレーム時に実行されるループイベントです
+      function tick() {
+        box.rotation.y += 0.01;
+        renderer.render(scene, camera); // レンダリング
+
+        requestAnimationFrame(tick);
+      }
+    }
