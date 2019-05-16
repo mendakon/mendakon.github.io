800ec92d5d8fb2fc2627de5a0d3bb73c31d59e67
mendako <v1758219@oita-u.ac.jp>
2019-05-16 10:56:04 +0900

    add: tree.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,7 @@
 "use strict"
 
 window.onload = function(){
-    
+  let mainCanvas = document.getElementById("mainCanvas")
+  let mainCanvasContext = mainCanvas.getContext("2d")
+  mainCanvasContext.fillText("hello world", 0, 60)
 }
